b253b881c04f8ff1e5a51ca10b44d769dcb0a362
GabrielDe Oliveira Lima <biellima.oliver28@gmail.com>
2025-03-04 22:50:28 -0300

    atualização

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let amigos= []
-let sorteio= []
 
 function adicionarAmigo(){
-    let inputAmigo = documentGetById("Amigo");
+    let inputAmigo = document.getElementById("Amigo");
     let nomeAmigo = input.value;
 
     if(nome === "" ) {
@@ -12,29 +11,30 @@ function adicionarAmigo(){
 }
 
 amigos.push(nomeAmigo);
-imputAmigo = "";
+inputAmigo = "";
 inputAmigo.focus();
- 
+
 function atualizarLista() {
-    let listaAmigos = documentGetById( "listaAmigos");
+    let listaAmigos = document.getElementById( "listaAmigos");
     listaAmigos.inner.HTML = "";
 }
  
 for (let i = 0; i < listaAmigos.length; i++);let item = document.createElement(li);
 item.textContent = amigos[i].text;
-listItem.appendChild(item);
+listaItem.appendChild(item);
 
 function sortearAmigo() {
-    if(amigosmigo.length ===0) {
+    if(amigos.length ===0) {
         alert("Adicione um nome");
         return;
-    }
+  }
 }
+
 let sorteado = amigos[Match.floor(Match.random() * amigos.length)];
-let resultado = documentGetById("Resultado");
+let resultado = document.getElementById("resultado");
 resultado.innerHTML = `O seu amigo secreto é: ${sorteado}`;
 
-let limpaLista = documentGetById()("listaAmigos");
+let limpaLista = document.getElementById()("listaAmigos");
     limparLista.innerHTML = "";
 
 
