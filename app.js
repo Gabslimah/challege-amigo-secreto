78730c9cc233cc01693427b30510617c44767132
Gabriel de Oliveira Lima <biellima.oliver28@gmail.com>
2025-03-10 18:37:11 -0300

    Ajuste fino

--- app.js
+++ app.js
@@ -6,55 +6,49 @@
     const input = document.getElementById("amigo");
     let nome = input.value.trim();
 
-  if (nome === ""|| !isNaN(nome)) {
-    alert("Insira pelo menos um nome");
-    return;
-   }
+    if (nome === "" || !isNaN(nome)) {
+       alert("Insira pelo menos um nome");
+       return;
+    }
 
     amigos.push(nome);
     atualizarLista();
 
     input.value = "";
-}   
+ }
 
  function atualizarLista() {
     const lista = document.getElementById('listaAmigos');
     (lista.innerHTML = ""),
     amigos.forEach((amigo, index) => {
-    const li = document.createElement("li"); 
-    li.textContent = amigo +  (index < amigos.length -1 ? "," : "");
-    lista.appendChild(li);
-});
-}
+       const li = document.createElement("li");
+       li.textContent = amigo + (index < amigos.length - 1 ? "," : "");
+       lista.appendChild(li);
+    });
+ }
 
  function sortearAmigo() {
     if (amigos.length === 0) {
        alert("Nenhum nome inserido, para jogar insira um nome válido.");
        return;
     }
-     
+
     if (amigos.length <= 2) {
-        alert("É necessário um número maior de participantes");
-        return;
+       alert("É necessário um número maior de participantes");
+       return;
     }
- 
-   
- let nomeSorteado = Math.floor(Math.random() * amigos.length);
-
- let amigoSorteado = amigos[nomeSorteado];
-
- sorteados.push(amigoSorteado);
-
- const resultado = document.getElementById("resultado");
- resultado.innerHTML = "O seu amigo secreto é: " + sorteados;
- 
-  atualizarLista();
-   
-
-   }
 
 
+    let nomeSorteado = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[nomeSorteado];
+
+    sorteados.push(amigoSorteado);
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "O seu amigo secreto é: " + sorteados;
+
+    atualizarLista();
 
 
-
-
+ }
